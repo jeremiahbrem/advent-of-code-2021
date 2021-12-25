be78eb95c553598833ffa9d29ab3941cabfd42c3
Jeremiah Brem <jeremiah.brem@gmail.com>
2021-12-25 02:31:14 -0600

    cleanup tests

--- typescript/day16/day16.test.ts
+++ typescript/day16/day16.test.ts
@@ -26,49 +26,49 @@ import {
 
 const sample = `D2FE28`;
 
-xdescribe('convertToBinary', () => {
+describe('convertToBinary', () => {
   it('converts hex to binary', () => {
     expect(convertToBinary(sample)).toBe('110100101111111000101000');
   });
 });
-xdescribe('getVersionNumber', () => {
+describe('getVersionNumber', () => {
   it('retrieves version number from binary', () => {
     const binary = '110100101111111000101000';
     expect(getVersionNumber(binary)).toBe(6);
   });
 });
-xdescribe('getPacketTypeId', () => {
+describe('getPacketTypeId', () => {
   it('retrieves packet type id from binary', () => {
     const binary = '110100101111111000101000';
     expect(getPacketTypeId(binary)).toBe(4);
   });
 });
-xdescribe('getFourBits', () => {
+describe('getFourBits', () => {
   it('returns four bits after prefix', () => {
     const binary = '10111';
     expect(getFourBits(binary)).toBe('0111');
   });
 });
-xdescribe('isLast', () => {
+describe('isLast', () => {
   it('determines if group is the last to parse', () => {
     const groups = [{bin: '10111', expected: false},{bin: '00101', expected: true}];
     groups.forEach(({bin, expected}) => expect(isLast(bin)).toBe(expected));
   });
 });
-xdescribe('extractGroups', () => {
+describe('extractGroups', () => {
   it('extracts bit groups from packet', () => {
     expect(extractGroups('101111111000101000')).toEqual([
       '0111', '1110', '0101'
     ]);
   });
 });
-xdescribe('isLiteralValuePacket', () => {
+describe('isLiteralValuePacket', () => {
   it('determines if packet is a literal value or not (operator packet)', () => {
     expect(isLiteralValuePacket('110100101111111000101000')).toBeTruthy();
     expect(isLiteralValuePacket('00111000000000000110111101000101001010010001001000000000')).toBeFalsy();
   });
 });
-xdescribe('getLiteralValue', () => {
+describe('getLiteralValue', () => {
   it('returns literal decimal value from bit groups', () => {
     const groups = [
       '0111', '1110', '0101'
@@ -76,48 +76,48 @@ xdescribe('getLiteralValue', () => {
     expect(getLiteralValue(groups)).toBe(2021);
   });
 });
-xdescribe('parseLiteralValuePacket', () => {
+describe('parseLiteralValuePacket', () => {
   it('returns represented decimal value from packet', () => {
     expect(parseLiteralValuePacket('110100101111111000101000')).toBe(2021);
   });
 });
-xdescribe('getLiteralValueLength', () => {
+describe('getLiteralValueLength', () => {
   it('returns length of literal value packet', () => {
     expect(getLiteralValueLength('11010001010')).toBe(11);
     expect(getLiteralValueLength('1101001100101010')).toBe(16);
   });
 });
-xdescribe('getLengthTypeId', () => {
+describe('getLengthTypeId', () => {
   it('returns length type id of operator packet', () => {
     expect(getLengthTypeId('00111000000000000110111101000101001010010001001000000000')).toBe('0');
     expect(getLengthTypeId('11101110000000001101010000001100100000100011000001100000')).toBe('1');
   });
 });
-xdescribe('getSubPacketsLength', () => {
+describe('getSubPacketsLength', () => {
   it('returns total length of subpackets', () => {
     expect(getSubPacketsLength('00111000000000000110111101000101001010010001001000000000')).toBe(27);
   });
 });
-xdescribe('getSubPacketsCount', () => {
+describe('getSubPacketsCount', () => {
   it('returns total length of subpackets', () => {
     expect(getSubPacketsCount('11101110000000001101010000001100100000100011000001100000')).toBe(3);
   });
 });
-xdescribe('getLengthTypeSubstring', () => {
+describe('getLengthTypeSubstring', () => {
   it('returns substring of subpackets from length type operator', () => {
     expect(getLengthTypeSubstring('00111000000000000110111101000101001010010001001000000000', 27)).toEqual(
       '110100010100101001000100100'
     );
   });
 });
-xdescribe('getCountTypeSubstring', () => {
+describe('getCountTypeSubstring', () => {
   it('returns substring of subpackets from count type operator', () => {
     expect(getCountTypeSubstring('11101110000000001101010000001100100000100011000001100000')).toEqual(
       '01010000001100100000100011000001100000'
     );
   });
 });
-xdescribe('unPackLengthTypeSubPackets', () => {
+describe('unPackLengthTypeSubPackets', () => {
   it('returns array of subpackets', () => {
     const totalPackets: string[] = [];
     const testSample = '00111000000000000110111101000101001010010001001000000000';
@@ -129,7 +129,7 @@ xdescribe('unPackLengthTypeSubPackets', () => {
     expect(totalPackets).toEqual(operatorPacket.subPackets);
   });
 });
-xdescribe('unPackCountTypeSubPackets', () => {
+describe('unPackCountTypeSubPackets', () => {
   it('returns array of subpackets', () => {
     const totalPackets: string[] = [];
     const testSample = '11101110000000001101010000001100100000100011000001100000';
@@ -141,7 +141,7 @@ xdescribe('unPackCountTypeSubPackets', () => {
     expect(totalPackets).toEqual(operatorPacket.subPackets);
   });
 });
-xdescribe('addLiteralValueType', () => {
+describe('addLiteralValueType', () => {
   it('adds literal value type subpacket to array of subpackets', () => {
     const totalPackets: string[] = [];
     const operatorPacket = { packet: "", subPackets: [] };
@@ -152,7 +152,7 @@ xdescribe('addLiteralValueType', () => {
     expect(totalPackets).toEqual(operatorPacket.subPackets);
   });
 });
-xdescribe('parseLengthTypeOperator', () => {
+describe('parseLengthTypeOperator', () => {
   it('retrieves all operator subpackets', () => {
     const totalPackets: string[] = [];
     const testSample = '00111000000000000110111101000101001010010001001000000000';
@@ -162,7 +162,7 @@ xdescribe('parseLengthTypeOperator', () => {
     expect(totalPackets).toEqual(operatorPacket.subPackets);
   });
 });
-xdescribe('parseCountTypeOperator', () => {
+describe('parseCountTypeOperator', () => {
   it('retrieves all operator subpackets', () => {
     const totalPackets: string[] = [];
     const testSample = '11101110000000001101010000001100100000100011000001100000';
@@ -172,12 +172,12 @@ xdescribe('parseCountTypeOperator', () => {
     expect(totalPackets).toEqual(operatorPacket.subPackets);
   });
 });
-xdescribe('parseOperatorPacket', () => {
+describe('parseOperatorPacket', () => {
   it('retrieves all length type operator subpackets', () => {
     const totalPackets: string[] = [];
     const testSample = '00111000000000000110111101000101001010010001001000000000';
     const operatorPacket = { packet: testSample, subPackets: [] };
-    const length = parseOperatorPacket(operatorPacket, totalPackets);
+    const { packetLength } = parseOperatorPacket(operatorPacket, totalPackets);
     expect(operatorPacket.subPackets).toEqual([
       '11010001010',
       '0101001000100100']);
@@ -185,13 +185,13 @@ xdescribe('parseOperatorPacket', () => {
       '00111000000000000110111101000101001010010001001000000000',
       ...operatorPacket.subPackets
       ]);
-    expect(length).toBe(49);
+    expect(packetLength).toBe(49);
   });
   it('retrieves all count type operator subpackets', () => {
     const totalPackets: string[] = [];
     const testSample = '11101110000000001101010000001100100000100011000001100000';
     const operatorPacket = { packet: testSample, subPackets: [] };
-    const length = parseOperatorPacket(operatorPacket, totalPackets);
+    const { packetLength } = parseOperatorPacket(operatorPacket, totalPackets);
     expect(operatorPacket.subPackets).toEqual([
       '01010000001',
       '10010000010',
@@ -200,12 +200,12 @@ xdescribe('parseOperatorPacket', () => {
       '11101110000000001101010000001100100000100011000001100000',
       ...operatorPacket.subPackets
      ]);
-    expect(length).toBe(51);
+    expect(packetLength).toBe(51);
   });
 });
 
 
-xdescribe('day 15 part 1', () => {
+describe('day 15 part 1', () => {
   it('returns sum of versions', () => {
     expect(partOne('38006F45291200')).toBe(9);
     expect(partOne('EE00D40C823060')).toBe(14);
